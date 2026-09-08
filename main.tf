bc40ef95b7a462eb7e1a17f02c93f8b6820f8e83
AVMUser-64851638@LODSPRODMCA.onmicrosoft.com <AVMUser-64851638@LODSPRODMCA.onmicrosoft.com>
2026-09-08 03:38:52 -0700

    Added tags to resource group

--- main.tf
+++ main.tf
@@ -12,8 +12,14 @@ provider "azurerm" {
   resource_provider_registrations = "core"
 }
  
- 
 resource "azurerm_resource_group" "contoso_rg" {
   name     = "contoso_rg"
   location = "UK South"
+
+  tags = {
+    cost_center = "contoso research"
+  }
 }
+
+
+
